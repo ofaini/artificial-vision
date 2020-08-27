fc0033fb4e407276459f57b9428b778dfecd4b64
ofaini <oscar.faini@gmail.com>
2020-08-27 12:13:45 -0500

    Upload myAge script

---
+++ myAge.py
@@ -0,0 +1,12 @@
+#Cometarios de línea
+#Developer: Oscar Faini
+
+'''
+Comentario de bloque
+'''
+myName = "Oscar Faini"
+myDate = 1987
+year = 2020
+age = year-myDate
+
+print ("Your name is: ",myName," you are ",age, " years old")
